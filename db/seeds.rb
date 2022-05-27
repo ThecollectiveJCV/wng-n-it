9b9848ea356216540333f8d0950dd5991796c502
joagui83 <jjaggie83@gmail.com>
2022-05-27 10:40:29 -0700

    adding changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@ require 'faker'
         id: id,
         name: Faker::Restaurant.name, 
         zip: Faker::Address.zip, 
-        img:Faker::LoremPixel.image(size: "50x60"), 
+        img: Faker::LoremPixel.image(size: "50x60"), 
         avg_rating: rand(1..5)
         )
 end
@@ -13,7 +13,14 @@ end
 
 user = User.where(email: 'test@test.test').first_or_create(password: '12345678', password_confirmation: '12345678')
 
-review.each do |each_review|
-    user.reviews.create each_review
-    puts "creating review #{each_review}"
+
+
+50.times do |id, restaurant_id|
+    Review.create(
+        id: id,
+        restaurant_id: restaurant_id,
+        img: Faker::LoremPixel.image(size: "50x60"), 
+        rating: rand(1..5)
+        )
 end
+
