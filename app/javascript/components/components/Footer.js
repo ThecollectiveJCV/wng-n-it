02cb69afa92eb9c0f8cfe6ade720629f7ad913f8
VicenteSaldana1 <vsaldana210@gmail.com>
2022-06-07 15:06:20 -0700

    footer edits

--- app/javascript/components/components/Footer.js
+++ app/javascript/components/components/Footer.js
@@ -5,7 +5,7 @@ export default class Footer extends Component {
     return (
       <div className= "footer">
         <h3 >&copy; Jose Aguilar, Vicente Saldana, and CJ Priedel.</h3>
-        <h3>Learn Academy</h3>
+        <h3>The Collective. </h3>
         </div>
     )
   }
